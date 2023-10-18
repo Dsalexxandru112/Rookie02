a6469f6cb0659789f337ed3e8ae75ceb18035f33
Alex <suteualexandru1278@gmail.com>
2023-10-18 20:51:10 +0300

    ok

--- src/exercitii.java
+++ src/exercitii.java
@@ -3,8 +3,17 @@ public class exercitii
     public static void main(String[] args)
     {
         //ex1
+        for (int i=0; i<=10; i++){
+            System.out.println(i);
+        }
 
+        for (int k=20; k>=10; k--){
+            System.out.println(k);
+        }
 
+        for(int l=0; l<=100; l++){
+            System.out.println(l);
+        }
 
 
     }
